f4c6e1566e759df262eafd834dccda309ad0d6ac
unknown <robonile4@gmail.com>
2024-12-03 08:23:45 +0545

    hello

---
+++ Models/Student.cs
@@ -0,0 +1,11 @@
+﻿public class Student
+{
+    public string StudentID { get; set; }      
+    public string FirstName { get; set; }      
+    public string LastName { get; set; }         
+    public string Email { get; set; }            
+    public DateTime EnrollmentDate { get; set; } 
+    public string Address { get; set; }         
+    public DateTime CreatedDate { get; set; }   
+
+}
